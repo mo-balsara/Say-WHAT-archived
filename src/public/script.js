ece477663f6a6adb3b58af3dd3f469898d9ec9d5
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-02-25 11:29:55 -0500

    css, new buttons,

--- src/public/script.js
+++ src/public/script.js
@@ -1,5 +1,24 @@
+function createdivs(data){
+    let posts = String(data)
+    posts = (posts.split(', '))
+    for (var i of posts){
+        if (i == 'None'){
+            continue;
+        }
+        let div = document.createElement('div')
+
+        
+    }
+    
+}
+
+
 function refresh(){
     fetch('https://saywhatserver.lamaqdahodwala.repl.co/newestposts')
     .then(response => response.text())
-    .then(data => console.log(data))
+    .then(data => createdivs(data))
+}
+
+function post(){
+    
 }
