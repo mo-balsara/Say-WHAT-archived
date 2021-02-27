4d339a2051be75c3719517c5de7d19bc8a46ea01
Lamaq Dahodwala <ldahodwala@wtsdnj.com>
2021-02-27 17:40:23 +0000

    Posting finished

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Say WHAT!
 is a dynamic forum that is kind of similar to reddit, but you share your mind boggling storys on it.
+
---
+++ script.js
@@ -0,0 +1,73 @@
+function createdivs(data){
+    let posts = String(data)
+    posts = (posts.split(', '))
+    for (var i of posts){
+        if (i == 'None'){
+            continue;
+        }
+        let div = document.createElement('div')
+        console.log(i)
+
+        
+    }
+    console.log('request sent')
+    
+}
+
+function sendtoserver(){
+    let data = {
+        title: document.getElementsByTagName('textarea')[0].value,
+        author: document.getElementsByTagName('textarea')[1].value,
+        story: document.getElementsByTagName('textarea')[2].value
+    }
+    
+    var map = new Map(Object.entries(JSON.parse(JSON.stringify(data))))
+    try{
+        for (var i of map.values()){
+            if (i.length < 10){
+                throw new Error()
+            }
+        }
+    } catch {
+        alert('One of your text boxes is too short.')
+        return;
+    }
+    
+    fetch('https://saywhatserver.lamaqdahodwala.repl.co/post', {
+        method: "POST", 
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        cache: 'no-cache',
+        body: JSON.stringify(data)
+        
+    })
+    .then(alert('Post created!'))
+}
+
+function refresh(){
+    
+    fetch('https://saywhatserver.lamaqdahodwala.repl.co/newestposts')
+    .then(response => response.text())
+    .then(data => createdivs(data))
+
+}
+
+function poststory(){
+    unhide('postscreen')
+    hide('thingstodo')
+    hide('title')
+}
+
+
+
+
+
+
+function hide(element){
+    document.getElementById(element).hidden = true
+}
+
+function unhide(element){
+    document.getElementById(element).hidden = false
+}
