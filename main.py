ecdb471bac5f3c54a6c9366c7de4c840d6cb564b
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-17 21:22:37 -0400

    new stuff and stuff

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ def home():
 
 @app.route('/read/<id>')
 def read(id):
-    pass
+    resp = requests.get('https://saywhatserver.lamaqdahodwala.repl.co')
+    content = resp.text
+    return content
 app.run('127.0.0.1', port=5555)
